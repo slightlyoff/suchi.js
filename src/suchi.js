819b3b6d075ff789a38a6869f49264662adc4b29
Alex Russell <slightlyoff@chromium.org>
2013-02-17 18:43:39 +0000

    It's a brave new world...Safari on Windows is dead-end, IE9 is no-longer current, and Chrome is up to 24. Wowza.

--- src/suchi.js
+++ src/suchi.js
@@ -3,7 +3,7 @@
  *
  * Suchi.js -- No user left behind.
  *
- * Copyright 2012:
+ * Copyright 2012-2013:
  *      Alex Russell <slightlyoff@chromium.org>
  *      Frances Berriman <phaeness@gmail.com>
  *
@@ -28,7 +28,7 @@
 
   /*
    * There is no feature test for staleness.
-   * 
+   *
    * Tight regular expressions to test for the most prevalant stale browsers as
    * reported by:
    *  http://marketshare.hitslink.com/browser-market-share.aspx?qprid=2&qpcustomd=0
@@ -40,59 +40,96 @@
    * Browsers are removed from the list as they fall below 1% global share.
    */
   suchi.laggards = {
-    // IE 8:     26%
-    IE80: /^Mozilla\/4\.0 \(compatible; MSIE 8\.0; Windows NT \d\.\d(.*)\)$/g,
 
-    // IE 7:      4%
-    IE70: /^Mozilla\/4\.0 \(compatible; MSIE 7\.0; Windows NT \d\.\d(.*)\)$/g,
+    // IE 9:     XX%
+    // Mozilla/5.0 (Windows; U; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727)
+    // IE9: /^Mozilla\/5\.0 \(compatible; MSIE 9\.0; Windows NT \d\.\d(.*); Trident\/5\.0(.*)\)$/g,
+    IE9: /^Mozilla\/5\.0 \(compatible; MSIE 9\.0; Windows NT \d\.\d(.*)\)$/g,
 
-    // IE 6:      7%
-    IE60: /^Mozilla\/4\.0 \(compatible; MSIE 6\.0; Windows NT \d\.\d(.*)\)$/g,
-    
-    // FF 3.6:    2%
+    // IE 8:     XX%
+    // IE8: /^Mozilla\/4\.0 \(compatible; MSIE 8\.0; Windows NT \d\.\d;(.*)? Trident\/4\.0(;)?(.*)\)$/g,
+    IE8: /^Mozilla\/4\.0 \(compatible; MSIE 8\.0; Windows NT \d\.\d(.*)\)$/g,
+
+    // IE 7:      X%
+    // FIXME: test for Trident version #
+    IE7: /^Mozilla\/4\.0 \(compatible; MSIE 7\.0; Windows NT \d\.\d(.*)\)$/g,
+
+    // IE 6:      X%
+    // FIXME: test for Trident version #
+    IE6: /^Mozilla\/4\.0 \(compatible; MSIE 6\.0; Windows NT \d\.\d(.*)\)$/g,
+
+    // FF 3.6:    X%
     // Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6;en-US; rv:1.9.2.9) Gecko/20100824 Firefox/3.6.9
     FF36: /^Mozilla\/5\.0 \((Windows NT|Macintosh); U;(.*)rv\:1\.9\.2.(\d{1,2})\)( Gecko\/(\d{8}))? Firefox\/3\.6(\.\d{1,2})?( \(.+\))?$/g,
 
-    // Chrome 17: 2%
+    // Chrome 18: X%
+    CR18: /^Mozilla\/5\.0 \((Windows NT|Macintosh)(;)?( .*)\) AppleWebKit\/535\.19 \(KHTML, like Gecko\) Chrome\/18\.0\.\d{4}\.\d{1,2} Safari\/535\.19$/g,
+    // Chrome 17: X%
     // Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.66 Safari/535.11
     // Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.66 Safari/535.11
     CR17: /^Mozilla\/5\.0 \((Windows NT|Macintosh)(;)?( .*)\) AppleWebKit\/535\.11 \(KHTML, like Gecko\) Chrome\/17\.0\.\d{3}\.\d{1,2} Safari\/535\.11$/g,
 
-    // FF 10:     1%
+    // FF 10:     X%
     // Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:10.0) Gecko/20100101 Firefox/10.0
     FF10: /^Mozilla\/5\.0 \((Windows NT|Macintosh); (.*)rv\:10\.0(\.\d{1,2})?\) Gecko\/\d{8} Firefox\/10\.0(\.\d{1,2})?$/g,
     // FF 11: (no stats yet, but for good measure)
     FF11: /^Mozilla\/5\.0 \((Windows NT|Macintosh); (.*)rv\:11\.0(\.\d{1,2})?\) Gecko\/\d{8} Firefox\/11\.0(\.\d{1,2})?$/g
+
+    // FIXME(slightlyoff): need to add mobile laggads, but we need to
+    // differentiate by OS in order to offer cogent choices
+    //
+    // Information sources we can mine to make these choices:
+    //    http://developer.android.com/about/dashboards/index.html
+    //      - looks like 2.3 is really the big issue
+    //    http://goo.gl/7yfNy
+    //      -
+    //
   };
 
   var CHROME = "CHROME",
       GCF    = "GCF",
       FF     = "FF",
       SAFARI = "SAFARI",
-      IE     = "IE";
+      IE     = "IE",
+      OPERA  = "OPERA";
 
   suchi.options = {
-    IE80: {
-      "xp":    [     CHROME, FF, SAFARI, GCF ],
-      "vista": [ IE, CHROME, FF, SAFARI, GCF ],
-      "win7":  [ IE, CHROME, FF, SAFARI, GCF ]
+    IE9: {
+      "vista": [     CHROME, FF, GCF, OPERA ],
+      "win7":  [ IE, CHROME, FF, GCF, OPERA ]
     },
-    IE70: {
-      "xp":    [     CHROME, FF, SAFARI, GCF ],
-      "vista": [ IE, CHROME, FF, SAFARI, GCF ]
+    IE8: {
+      "xp":    [     CHROME, FF, GCF, OPERA ],
+      "vista": [ IE, CHROME, FF, GCF, OPERA ],
+      "win7":  [ IE, CHROME, FF, GCF, OPERA ]
     },
-    IE60: {
-      "xp":    [     CHROME, FF, SAFARI, GCF ]
+    IE7: {
+      "xp":    [     CHROME, FF, GCF, OPERA ],
+      "vista": [ IE, CHROME, FF, GCF, OPERA ]
+    },
+    IE6: {
+      "xp":    [     CHROME, FF, GCF, OPERA ]
     }
+    // FIXME: need to add mobile dead-ends/options here!
+    // Mobile dead-enders that we care about:
+    //  Android Browser
+    //  Safari on devices that won't get updates
+    //    - only evergreen iOS option is a proxy-browser, e.g. Opera Mini
+    // Mobile evergreen options:
+    //  Opera
+    //  Chrome (For ICS+ Android users)
+    //  FF: for ARMv7+ devices (see: http://goo.gl/PhQs9)
+    // Inclusive-design mobile browsers (no hope, just fallbacks):
+    //
   };
 
-  var portable = [ "FF36", "CR17", "FF10", "FF11" ];
+  var portable = [ "FF36", "CR18", "CR17", "FF10", "FF11" ];
   for(var x = 0; x < portable.length; x++) {
     suchi.options[portable[x]] = {
-      "xp":    [     CHROME, FF, SAFARI ],
-      "vista": [ IE, CHROME, FF, SAFARI ],
-      "win7":  [ IE, CHROME, FF, SAFARI ],
-      "osx":   [     CHROME, FF, SAFARI ]
+      "xp":    [     CHROME, FF, OPERA ],
+      "vista": [ IE, CHROME, FF, OPERA ],
+      "win7":  [ IE, CHROME, FF, OPERA ],
+      "osx":   [     CHROME, FF, OPERA ]
     };
   }
 
--- tests/ua-tests.js
+++ tests/ua-tests.js
@@ -23,57 +23,109 @@
 (function() {
 "use strict";
 
+// Chrome 24
+var CR24_WIN8 = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.15 (KHTML, like Gecko) Chrome/24.0.1295.0 Safari/537.15";
+var CR24_OSX = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17";
+
+// CHrome 18
 var CR18_VISTA = "Mozilla/5.0 (Windows NT 6.0; WOW64) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.45 Safari/535.19";
 var CR18_OSX = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.45 Safari/535.19";
+
+// Chrome 17
+var CR17_XP = "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.66 Safari/535.11";
+var CR17_OSX = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.66 Safari/535.11";
+
+// IE 10
+var IE10_generic = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)";
+var IE10_WIN8 = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0; Touch)";
+var IE10_WIN7 = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)";
+
+// IE 9
+var IE9_generic = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)";
 var IE9_WIN7 = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)";
 var IE9_WIN72 = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; Zune 4.0; InfoPath.3; MS-RTC LM 8; .NET4.0C; .NET4.0E)";
 var IE9_WIN7_GCF = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0; chromeframe/13.0.782.215)";
 
+  // IE 8
+var IE8_WIN7 = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0)";
+var IE8_WIN7_MC =  "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; Media Center PC 6.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C)";
+var IE8_WIN7_crufty = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.3; .NET4.0C; .NET4.0E; .NET CLR 3.5.30729; .NET CLR 3.0.30729; MS-RTC LM 8)";
+var IE8_WIN_VISTA = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; InfoPath.1; SV1; .NET CLR 3.8.36217; WOW64; en-US)";
+var IE8_WIN_XP_64 = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Trident/4.0; Media Center PC 4.0; SLCC1; .NET CLR 3.0.04320)";
+var IE8_WIN_XP = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; SLCC1; .NET CLR 1.1.4322)";
+var IE8_WIN_XP_crufty = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; MS-RTC LM 8; InfoPath.3; .NET4.0C; .NET4.0E; chromeframe/13.0.782.215)";
+
+// IE 7
+var IE7_WIN_XP = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727)";
+var IE7_VISTA = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; en-US)";
+var IE7_WIN2K3 = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.2; .NET CLR 1.1.4322)";
+var IE7_WIN_XP_Avant = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Avant Browser; .NET CLR 2.0.50727; Creative ZENcast v1.02.12; .NET CLR 3.0.04506.30)";
+var IE7_WIN_XP_Toolbar = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 2.0.50727; MEGAUPLOAD 1.0)"
+
+// IE 6
+var IE6_WIN2K = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)";
+var IE6_XP = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; Q312461)";
+var IE6_XP_SP2 = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322; .NET CLR 2.0.40607)";
+var IE6_WIN_XP_64 = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)";
+var IE6_XP_NeoPlanet = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; NeosBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)";
+
 var current = [
+  CR24_WIN8,
+  CR24_OSX,
+
+  IE10_generic,
+  IE10_WIN8,
+  IE10_WIN7,
+
+  // FIXME: FF18, Safari 6, etc.
+];
+
+var laggards = [
   // Chrome 18
   CR18_VISTA,
+  CR18_OSX,
+
+  // Chrome 17
+  CR17_XP,
   CR18_OSX,
 
   // IE 9
   IE9_WIN7,
   IE9_WIN72,
-  IE9_WIN7_GCF
-
-  // FIXME: FF12, Safari 5.1, etc.
-];
-
-var laggards = [
-  // Chrome 17
-  "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.66 Safari/535.11",
-  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.66 Safari/535.11",
+  IE9_WIN7_GCF,
 
   // IE 8
-  "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; Media Center PC 6.0; InfoPath.2; MS-RTC LM 8)",
-  "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; Media Center PC 6.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C)",
-  "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.3; .NET4.0C; .NET4.0E; .NET CLR 3.5.30729; .NET CLR 3.0.30729; MS-RTC LM 8)",
-  "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)",
-  "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; Zune 3.0)",
+  IE8_WIN7,
+  IE8_WIN7_MC,
+  IE8_WIN7_crufty,
+  IE8_WIN_VISTA,
+  IE8_WIN_XP_64,
+  IE8_WIN_XP,
+  IE8_WIN_XP_crufty,
 
   // IE 7
-  "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.2; .NET CLR 1.1.4322)",
-  "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; GTB6.4; .NET CLR 1.1.4322; FDM; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)",
-  "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Avant Browser; .NET CLR 2.0.50727; Creative ZENcast v1.02.12; .NET CLR 3.0.04506.30)",
-  "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.2; Win64; x64; .NET CLR 2.0.50727)",
+  IE7_WIN_XP,
+  IE7_VISTA,
+  IE7_WIN2K3,
+  IE7_WIN_XP_Avant,
+  IE7_WIN_XP_Toolbar,
 
   // IE 6
-  "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; NeosBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
-  "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+  IE6_XP,
+  IE6_XP_SP2,
+  IE6_WIN_XP_64,
+  IE6_XP_NeoPlanet,
 ];
 
+current.forEach(function(c) {
+  doh.add("User Agent", "t.f(suchi.isBehind(\n\t\"" + c + "\"));");
+});
+
+laggards.forEach(function(c) {
+  doh.add("User Agent", "t.t(suchi.isBehind(\n\t\"" + c + "\"));");
+});
+
 doh.add("User Agent", [
-
-  function current_isBehind(t) {
-    current.forEach(function(c) { t.f(suchi.isBehind(c)); });
-  },
-
-  function laggards_isBehind(t) {
-    laggards.forEach(function(c) { t.t(suchi.isBehind(c)); });
-  },
 
   function options(t) {
   },
